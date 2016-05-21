f81cab67d3432cc20a45a505a19b9e2fe62a3d1b
Xavier Gerber <xavier1.gerber@gmail.com>
2016-05-21 15:31:20 +0200

    Fix bug

--- app/src/main/java/asyncTasks/SendQueueMessage.java
+++ app/src/main/java/asyncTasks/SendQueueMessage.java
@@ -2,7 +2,10 @@ package asyncTasks;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -24,6 +27,18 @@ public class SendQueueMessage extends AsyncTask<String, Void, Void> {
             DataOutputStream os = new DataOutputStream(httpcon.getOutputStream());
             os.write(("author="+pseudo+"&message=\""+message+"\"").getBytes("UTF-8"));
             os.close();
+
+            InputStream inputStream = httpcon.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = reader.readLine();
+            if(line == null){
+                return null;
+            }
         }
         catch (Exception e){
 
